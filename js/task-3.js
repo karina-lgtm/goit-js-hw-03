c1be2031ef3f420f91dc3b69cd2e90a9b3570be7
Karina Myronova <km0023549@gmail.com>
2024-12-28 19:47:38 +0100

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,30 @@
+function getElementWidth(content, padding, border) {
+    
+    const contentWidth = parseFloat(content);
+    const paddingWidth = parseFloat(padding);
+    const borderWidth = parseFloat(border);
+    
+   
+    const totalWidth = contentWidth + (2 * paddingWidth) + (2 * borderWidth);
+    
+   
+    return totalWidth;
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+
+
+function checkForSpam(message) {
+    const lowerCaseMessage = message.toLowerCase();
+    return lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale");
+}
+
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Amazing Sale, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of SPAM emails. Our book is on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
