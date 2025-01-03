f1954b1f1113579ff72e184d1fe6e3190972f4ef
Karina Myronova <karinamyronova@MacBookAir.fritz.box>
2025-01-03 13:41:36 +0100

    hw3

--- js/task-1.js
+++ js/task-1.js
@@ -1,21 +1,3 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  const totalPrice = quantity * pricePerDroid;
-
-  if (totalPrice > customerCredits) {
-      return "Insufficient funds!";
-  } else {
-      return `You ordered ${quantity} droids worth ${totalPrice} credits.`;
-  }
-}
-
-
-console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits."
-console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits."
-console.log(makeTransaction(10, 500, 5000));  // "Insufficient funds!"
-console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
-console.log(makeTransaction(10, 500, 5000));  // "Insufficient funds!"
-
-
 function slugify(title) {
   return title
     .toLowerCase()               
@@ -27,7 +9,7 @@ function slugify(title) {
     .replace(/-+$/, '');           
 }
 
-// Перевірка роботи функції
+// Тестування функції
 console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
 console.log(slugify("English for developer")); // "english-for-developer"
 console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
